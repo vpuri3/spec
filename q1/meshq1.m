0cf6fecdad3ca35b7129816d107748f3a88197b7
Vedant Puri <vedantpuri@Vedants-MacBook-Pro.local>
2018-07-30 12:47:59 +0530

    comments

--- q1/meshq1.m
+++ q1/meshq1.m
@@ -12,29 +12,39 @@ G = zeros(ne,N*N,3); % G_11,G_12,G_22
 x = []; E = zeros(ne,N*N);
 xx = zeros(N*N,2);
 tol = 1e-12;
+
 for ei = 1:ne
   KK = EE(ei,:);
   Vx = XX(KK);
   Vy = YY(KK);
+
+  % interpolate a spectral mesh inside each elemnt
   xx(:,1)=0.25*sum(Vx)+0.5*(Vx(2)-Vx(1))*R+0.5*(Vx(4)-Vx(1))*S;
   xx(:,2)=0.25*sum(Vy)+0.5*(Vy(2)-Vy(1))*R+0.5*(Vy(4)-Vy(1))*S;
+
+  % Jacobian for each element
   dxdr = 0.5*(Vx(2)-Vx(1)); drdx = 1./dxdr;
   dxds = 0.5*(Vx(4)-Vx(1)); dsdx = 1./dxds;
   dydr = 0.5*(Vy(2)-Vy(1)); drdy = 1./dydr;
   dyds = 0.5*(Vy(4)-Vy(1)); dsdy = 1./dyds;
+
   if (min(abs(dxdr)) < tol) drdx(:) = 0; end;
   if (min(abs(dxds)) < tol) dsdx(:) = 0; end;
   if (min(abs(dydr)) < tol) drdy(:) = 0; end;
   if (min(abs(dyds)) < tol) dsdy(:) = 0; end;
+
   J(ei,:) = dxdr.*dyds-dydr.*dxds; J(ei,:) = abs(J(ei,:));
   G(ei,:,1) = (drdx.*drdx+drdy.*drdy).*J(ei,:);
   G(ei,:,2) = (drdx.*dsdx+drdy.*dsdy).*J(ei,:);
   G(ei,:,3) = (dsdx.*dsdx+dsdy.*dsdy).*J(ei,:);
-  % lexicographical ordering within each element.
+
+  % ensure lexicographical ordering within each element.
   if(ei==1) x = xx; end;
   x = [x; setdiff(xx,x,'rows')];
   [~,K] = ismember(xx,x,'rows','R2012a');
   E(ei,:) = K;
+
+
 end
 
 nv = length(x);
