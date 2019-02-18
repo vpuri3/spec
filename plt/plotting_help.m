f78c83241a90b31e010654f487ab3cc86e4ec72a
Vedant Puri <vedantpuri@wirelessprv-10-195-213-152.near.illinois.edu>
2019-02-17 22:01:42 -0600

    comments

--- plt/plotting_help.m
+++ plt/plotting_help.m
@@ -1,17 +1,19 @@
-% Plotting
-close all % Close all currently open figures
+
+				% Plotting
+close all;
 hold on % Plot on the same figure
-% Colors: b = blue, k = black, r = red, g = green, y = yellow
-% Styling: o = circles, - = solid line, -- = dashed line
+
+%%%%---------------------------------------------------------------
 plot(x,y,'bo',var,P,'r--')  % Plot...add as many x and y combinations as needed 
 legend('Data Points','Langrange Approximation') % names in order of plotting above
+% Colors: b = blue, k = black, r = red, g = green, y = yellow
+% Styling: o = circles, - = solid line, -- = dashed line
 
 
-loglog , semilogx , semilogy % logarithmic, semi-logarithmic plots
-hold on % between plot calls would create different figures.
+loglog , semilogx , semilogy % logarithmic, semi-logarithmic
 
 
-%% Plotting with loops.
+%% Plotting with loops eg:
 hold on
 for c = 1:d
     len = 5/h(c)+1;
@@ -22,3 +24,19 @@ xlabel('Time','FontSize',14)
 ylabel('Absolute Error','FontSize',14)
 axis sqaure
 legend('show')
+%%%%---------------------------------------------------------------
+% plotting
+figure; fig=gcf; ax=gca;
+ax.XScale='log';
+ax.YScale='linear';
+ax.FontSize=14;
+xlabel('y-plus')
+ylabel('u-plus')
+
+title(['SmoothWavyWall, atime: ' num2str(atime)],'fontsize',14)
+hold on; grid on; axis square;
+
+semilogx(yp(V),up(V),'ko','linewidth',2.0,'displayname','Valley');
+semilogx(yp(P),up(P),'ro','linewidth',2.0,'displayname','Peaks');
+
+legend('location','northwest')
