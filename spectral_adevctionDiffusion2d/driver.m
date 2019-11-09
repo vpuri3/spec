62c38d13c68d34dfd3fe2bc78ecef612a198c8f7
Vedant Puri <vpuri3@illinois.edu>
2019-11-08 20:24:24 -0600

    moved CG function into driver.m

--- spectral_adevctionDiffusion2d/driver.m
+++ spectral_adevctionDiffusion2d/driver.m
@@ -1,20 +1,18 @@
+%======================================================================
 %
-clf;
-format compact; format shorte;
-%----------------------------------------------------------------------
-%
-% steady state advection-diffusion equation
+%	Driver function for steady state advection diffusion solve
 %
 %	nu*\del^2 u + q*u + \vect{c}\dot\grad{u}  = f
 %   + Dirichlet/Neumann BC
 %
-% todo
-%     -fast diagonalization
+%======================================================================
+function driver
 %
-%----------------------------------------------------------------------
 
-nx1 = 16;
-ny1 = 16;
+clf; format compact; format shorte;
+
+nx1 = 64;
+ny1 = 3;
 nxd = ceil(1.5*nx1);
 nyd = ceil(1.5*ny1);
 
@@ -47,12 +45,14 @@ Js1d = interp_mat(zsmd,zsm1);
 
 [xm1,ym1] = ndgrid(zrm1,zsm1);
 [xmd,ymd] = ndgrid(zrmd,zsmd);
+
 %----------------------------------------------------------------------
 % data
 
-solve=1; % 0: CG (possion), 1: FDM (possion), 2: direct (advection-diffusion)
+% 0: CG (possion), 1: FDM (possion), 2: direct (advection-diffusion)
+solve=0;
 
-nu= 1e-0;
+nu= 1e-1;
 q = 0+0*xm1;
 cx= 0+0*xm1;
 cy= 0+0*xm1;
@@ -65,7 +65,7 @@ ub = 0-0*xm1;
 
 % mask
 Rx = Irm1(2:end-1,:);   % dir-dir
-Ry = Ism1(2:end-1,:);   % neu-neu
+Ry = Ism1(1:end-0,:);   % neu-neu
 msk = diag(Rx'*Rx) * diag(Ry'*Ry)';
 
 ub = (1-msk) .* ub; % step function on dirichlet BC
@@ -104,16 +104,19 @@ As = ABu(Ry,Ry,As);
 
 [Sr,Lr] = eig(Ar,Br); Sri = inv(Sr);
 [Ss,Ls] = eig(As,Bs); Ssi = inv(Ss);
-%for j=1:length(Sr); Sr(:,j) = Sr(:,j)/sqrt(Sr(:,j)'*Sr(:,j)); end; % scaling err
-%for j=1:length(Sr); Ss(:,j) = Ss(:,j)/sqrt(Ss(:,j)'*Ss(:,j)); end;
 Lfdm = diag(Lr) + diag(Ls)';
 
 %----------------------------------------------------------------------
+function [usys] =  system(vsys)
+	usys = nu*laplace2d(vsys,Jr1d,Js1d,Drm1,Dsm1,G11,G12,G22);
+	usys = usys + vsys.*q;
+	usys = usys + advect2d(vsys,cx,cy,Bmd,Irm1,Ism1,Jr1d,Js1d,Drm1,Dsm1,rxm1,rym1,sxm1,sym1);
+	usys = msk .* usys;
+end
+%----------------------------------------------------------------------
 % RHS
-b = mass2d(Bmd,Jr1d,Js1d,f);							  % forcing
-b = b - nu*laplace2d(ub,Jr1d,Js1d,Drm1,Dsm1,G11,G12,G22); % dirichlet bc
-b = b - ub.*q;
-b = b - advect2d(ub,cx,cy,Bmd,Irm1,Ism1,Jr1d,Js1d,Drm1,Dsm1,rxm1,rym1,sxm1,sym1);
+b = mass2d(Bmd,Jr1d,Js1d,f);	% forcing
+b = b - system(ub);				% dirichlet bc
 
 b = msk .* b;
 %----------------------------------------------------------------------
@@ -121,7 +124,7 @@ b = msk .* b;
 
 if(solve==0) % CG possion
 
-	[uh,iter,r2] = cg_possion2d(b,0*b,1e-8,500,Jr1d,Js1d,Drm1,Dsm1,G11,G12,G22,msk);
+	[uh,iter,r2] = cg_possion(b,0*b,1e-8,1e3);
 	['cg iter:',num2str(iter),', residual:',num2str(sqrt(r2))]
 
 elseif(solve==1) % FDM possion
@@ -176,9 +179,42 @@ end
 % BC
 
 u = uh + ub;
-
 %----------------------------------------------------------------------
 % plt
+
 mesh(xm1,ym1,u);
 xlabel('$$X$$');
 ylabel('$$Y$$');
+%----------------------------------------------------------------------
+
+%======================================================================
+%
+%	Conjugate Gradient
+%
+% 	from https://en.wikipedia.org/wiki/Conjugate_gradient_method
+%
+%======================================================================
+%
+%
+function [x,k,rsqnew] = cg_possion(b,x0,tol,maxiter);
+	x = x0;
+	r = b - system(x); % r = b - Ax
+	rsqold=dot2d(r,r);
+	
+	if(sqrt(rsqold) < tol); rsqnew=rsqold; return; end;
+	
+	p=r;
+	for k=1:maxiter
+		Ap = system(p); % Ap = A*p
+		al = rsqold / dot2d(p,Ap);
+		x  = x + al*p;
+		r  = r - al*Ap;
+		rsqnew=dot2d(r,r); if(sqrt(rsqnew) < tol); return; end;
+		be = rsqnew / rsqold;
+		p  = r + be*p;
+		rsqold = rsqnew;
+	end
+end
+%----------------------------------------------------------------------
+end % driver
+%----------------------------------------------------------------------
