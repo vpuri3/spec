0abccca3282ac4bd7d9047053c706c1954d4df6b
Vedant Puri <vpuri3@illinois.edu>
2019-11-09 13:19:12 -0600

    unsteady advection diffusion

--- spectral_adevctionDiffusion2d/driver.m
+++ spectral_adevctionDiffusion2d/driver.m
@@ -1,8 +1,9 @@
 %======================================================================
 %
-%	Driver function for steady state advection diffusion solve
+%	Driver function for advection diffusion equation
 %
-%	nu*\del^2 u + q*u + \vect{c}\dot\grad{u}  = f
+%	du/dt + \vect{c}\dot\grad{u}  = f + nu*\del^2 u + q*u 
+%
 %   + Dirichlet/Neumann BC
 %
 %======================================================================
@@ -11,8 +12,8 @@ function driver
 
 clf; format compact; format shorte;
 
-nx1 = 64;
-ny1 = 3;
+nx1 = 16;
+ny1 = 16;
 nxd = ceil(1.5*nx1);
 nyd = ceil(1.5*ny1);
 
@@ -49,29 +50,36 @@ Js1d = interp_mat(zsmd,zsm1);
 %----------------------------------------------------------------------
 % data
 
-% 0: CG (possion), 1: FDM (possion), 2: direct (advection-diffusion)
-solve=0;
+slv=1;                           % solver --> 0: CG, 1: FDM, 2: direct
 
-nu= 1e-1;
-q = 0+0*xm1;
-cx= 0+0*xm1;
+nu= 1e-1;                        % viscosity
+u = sin(pi*xm1).*sin(pi*ym1);    % initial condition
+cx= 0+0*xm1;                     % advecting field
 cy= 0+0*xm1;
-f = sin(pi*xm1).*sin(pi*ym1);
+f = sin(pi*xm1).*sin(pi*ym1);    % forcing
 f = 1+0*xm1;
-%ue = 
 
-% dirichlet BC
-ub = 0-0*xm1;
+ub = u; 					     % dirichlet BC
 
-% mask
-Rx = Irm1(2:end-1,:);   % dir-dir
-Ry = Ism1(1:end-0,:);   % neu-neu
-msk = diag(Rx'*Rx) * diag(Ry'*Ry)';
-
-ub = (1-msk) .* ub; % step function on dirichlet BC
+%Rx = Irm1(1:end-1,:); Rx(end,1)=1;  % periodic
+Rx = Irm1(2:end-1,:);                % dir-dir
+Ry = Ism1(2:end-1,:);                % dir-dir
+% time
+T   = 0; % steady state solve if T == 0
+CFL = 0.5;
 
 %----------------------------------------------------------------------
 % setup
+
+% time stepper
+dx = min(min(xm1));
+dt = dx*CFL/1;
+nt = floor(T/dt);
+dt = T/nt;
+
+% BDFk-EXTk setup
+a = zeros(3,1);
+b = zeros(4,1);
 
 % jacobian
 [Jm1,Jim1,rxm1,rym1,sxm1,sym1] = jac2d(xm1,ym1,Irm1,Ism1,Drm1,Dsm1);
@@ -81,64 +89,37 @@ ub = (1-msk) .* ub; % step function on dirichlet BC
 Bm1 = Jm1.*(wrm1*wsm1');
 Bmd = Jmd.*(wrmd*wsmd');
 
+% mask
+msk = diag(Rx'*Rx) * diag(Ry'*Ry)';
+
 % laplace operator setup
 G11 = Bmd .* (rxmd.*rxmd + rymd.*rymd);
 G12 = Bmd .* (rxmd.*sxmd + rymd.*symd);
 G22 = Bmd .* (sxmd.*sxmd + symd.*symd);
 
-% FDM - fast diagonalization setup
-Lx = max(max(xm1))-min(min(xm1));
-Ly = max(max(ym1))-min(min(ym1));
+if(slv==1) % fast diagonalization setup
 
-Br = (Lx/2)*diag(wrm1);
-Bs = (Ly/2)*diag(wsm1);
-Dr = (2/Lx)*Drm1;
-Ds = (2/Ly)*Dsm1;
-Ar = Dr'*Br*Dr;
-As = Ds'*Bs*Ds;
+	Lx = max(max(xm1))-min(min(xm1));
+	Ly = max(max(ym1))-min(min(ym1));
+	
+	Br = (Lx/2)*diag(wrm1);
+	Bs = (Ly/2)*diag(wsm1);
+	Dr = (2/Lx)*Drm1;
+	Ds = (2/Ly)*Dsm1;
+	Ar = Dr'*Br*Dr;
+	As = Ds'*Bs*Ds;
+	
+	Br = ABu(Rx,Rx,Br);
+	Bs = ABu(Ry,Ry,Bs);
+	Ar = ABu(Rx,Rx,Ar);
+	As = ABu(Ry,Ry,As);
+	
+	[Sr,Lr] = eig(Ar,Br); Sri = inv(Sr);
+	[Ss,Ls] = eig(As,Bs); Ssi = inv(Ss);
+	Lfdm = diag(Lr) + diag(Ls)';
+	Bm1i = 1 ./ Bm1;
 
-Br = ABu(Rx,Rx,Br);
-Bs = ABu(Ry,Ry,Bs);
-Ar = ABu(Rx,Rx,Ar);
-As = ABu(Ry,Ry,As);
-
-[Sr,Lr] = eig(Ar,Br); Sri = inv(Sr);
-[Ss,Ls] = eig(As,Bs); Ssi = inv(Ss);
-Lfdm = diag(Lr) + diag(Ls)';
-
-%----------------------------------------------------------------------
-function [usys] =  system(vsys)
-	usys = nu*laplace2d(vsys,Jr1d,Js1d,Drm1,Dsm1,G11,G12,G22);
-	usys = usys + vsys.*q;
-	usys = usys + advect2d(vsys,cx,cy,Bmd,Irm1,Ism1,Jr1d,Js1d,Drm1,Dsm1,rxm1,rym1,sxm1,sym1);
-	usys = msk .* usys;
-end
-%----------------------------------------------------------------------
-% RHS
-b = mass2d(Bmd,Jr1d,Js1d,f);	% forcing
-b = b - system(ub);				% dirichlet bc
-
-b = msk .* b;
-%----------------------------------------------------------------------
-% solve
-
-if(solve==0) % CG possion
-
-	[uh,iter,r2] = cg_possion(b,0*b,1e-8,1e3);
-	['cg iter:',num2str(iter),', residual:',num2str(sqrt(r2))]
-
-elseif(solve==1) % FDM possion
-
-	uh = (1/nu) * fdm(b,Bm1,Sr,Ss,Sri,Ssi,Rx,Ry,Lfdm);
-
-elseif(solve==2) % direct advection-diffusion
-
-	% data
-	q  = reshape(q, [nx1*ny1,1]); Q = sparse(diag(q ));
-	cx = reshape(cx,[nx1*ny1,1]);
-	cy = reshape(cy,[nx1*ny1,1]);
-	f  = reshape(f, [nx1*ny1,1]);
-	b  = reshape(b, [nx1*ny1,1]); % rhs
+elseif(slv==2) % exact solve setup
 
 	% operators
 	R  = sparse(kron(Ry,Rx));
@@ -157,35 +138,109 @@ elseif(solve==2) % direct advection-diffusion
 	A  = D'*G*D;
 
 	% advection op
+	Cx = reshape(cx,[nx1*ny1,1]);
+	Cy = reshape(cy,[nx1*ny1,1]);
 	RRX=sparse(diag(reshape(rxm1,[nx1*ny1,1])));
 	RRY=sparse(diag(reshape(rym1,[nx1*ny1,1])));
 	SSX=sparse(diag(reshape(sxm1,[nx1*ny1,1])));
 	SSY=sparse(diag(reshape(sym1,[nx1*ny1,1])));
-	Cxd=sparse(diag(J*cx));
-	Cyd=sparse(diag(J*cy));
+	Cxd=sparse(diag(J*Cx));
+	Cyd=sparse(diag(J*Cy));
 	Dx = RRX*Dr + SSX*Ds;
 	Dy = RRY*Dr + SSY*Ds;
 	C  = J'*Bd*(Cxd*J*Dx + Cyd*J*Dy);
 
-	% system
-	S  = R*(nu*A  + Q*B + C)*R';
-	b  = R*b;
-	uh = S \ b;
-	uh = R'*uh;
-	uh = reshape(uh,[nx1,ny1]);
+end
+
+%----------------------------------------------------------------------
+% LHS
+
+function [ulhs] =  lhs_op(vlhs)
+	ulhs = nu*laplace2d(vlhs,Jr1d,Js1d,Drm1,Dsm1,G11,G12,G22);
+	ulhs = ulhs + vlhs.*b(1);
+end
+%----------------------------------------------------------------------
+% RHS
+
+function [urhs] = rhs_op(vrhs)
+	urhs = -advect2d(vrhs,cx,cy,Bmd,Irm1,Ism1,Jr1d,Js1d,Drm1,Dsm1,rxm1,rym1,sxm1,sym1);
+	urhs = urhs + mass2d(Bmd,Jr1d,Js1d,f); % forcing
+	urhs = urhs - lhs_op(ub);              % dirichlet BC
+end
+%----------------------------------------------------------------------
+% time advance
+
+mesh(xm1,ym1,u);
+xlabel('$$x$$');
+ylabel('$$y$$');
+
+t  = 0;
+u0 = u*0;
+u1 = u0;
+u2 = u0;
+u3 = u0;
+f1 = u0;
+f2 = u0;
+
+for it=0:nt-1
+
+	% update time
+	t = t + dt;
+
+	if(it<=3)
+		[a,b] = bdfex3(i,dt);
+		Lfdmi = 1 ./ (b(1) + Lfdm);
+	end;
+	
+	% update histories
+	u3=u2; u2=u1; u1 = u;
+	f3=f2; f2=f1; f1 = rhs_op(u); % current u
+
+	% form BDF rhs
+	r  = a(1)*f1 +a(2)*f2 +a(3)*f3 - (b(2)*u1+b(3)*u2+b(4)*u3);
+	r  = msk .* r;
+
+	% solve
+	uh = solve(r);
+
+	u  = uh + ub;
+
+	if(mod(it,0.1*nt)==0);
+		hold off;
+		mesh(xm1,ym1,u); title(['t=',num2str(t)]);pause(0.05);
+	end
 
 end
 %----------------------------------------------------------------------
-% BC
+% solve
 
-u = uh + ub;
-%----------------------------------------------------------------------
-% plt
+function [uslv] = solve(rhs)
 
-mesh(xm1,ym1,u);
-xlabel('$$X$$');
-ylabel('$$Y$$');
-%----------------------------------------------------------------------
+	if(slv==0) % CG
+	
+		[uslv,iter,r2] = cg(rhs,0*r,1e-8,1e3);
+		['cg iter:',num2str(iter),', residual:',num2str(sqrt(r2))]
+	
+	elseif(slv==1) % FDM
+	
+		uslv = (1/nu) * fdm(rhs,Bm1i,Sr,Ss,Sri,Ssi,Rx,Ry,Lfdmi);
+	
+	elseif(slv==2) % direct solve
+	
+		rhs = reshape(rhs,[nx1*ny1,1]); % rhs
+
+		% system
+		S    = R*(nu*A + b(1)*B)*R';
+		rhs  = R*rhs;
+		uslv = S \ rhs;
+		uslv = R'*uslv;
+
+		uslv = reshape(uslv,[nx1,ny1]);
+		rhs  = reshape(rhs ,[nx1,ny1]);
+	
+	end
+
+end
 
 %======================================================================
 %
@@ -195,17 +250,16 @@ ylabel('$$Y$$');
 %
 %======================================================================
 %
-%
-function [x,k,rsqnew] = cg_possion(b,x0,tol,maxiter);
+function [x,k,rsqnew] = cg(b,x0,tol,maxiter);
 	x = x0;
-	r = b - system(x); % r = b - Ax
+	r = b - lhs_op(x); % r = b - Ax
 	rsqold=dot2d(r,r);
 	
 	if(sqrt(rsqold) < tol); rsqnew=rsqold; return; end;
 	
 	p=r;
 	for k=1:maxiter
-		Ap = system(p); % Ap = A*p
+		Ap = lhs_op(p); % Ap = A*p
 		al = rsqold / dot2d(p,Ap);
 		x  = x + al*p;
 		r  = r - al*Ap;
--- spectral_adevctionDiffusion2d/fdm.m
+++ spectral_adevctionDiffusion2d/fdm.m
@@ -1,11 +1,11 @@
 %
-function [u] = fdm(b,B,Sr,Ss,Sri,Ssi,Rx,Ry,D);
+function [u] = fdm(b,Bi,Sr,Ss,Sri,Ssi,Rx,Ry,Di);
 
-u = b ./ B;
-u = ABu(Ry ,Rx ,u);
-u = ABu(Ssi,Sri,u);
-u = u ./ D;
-u = ABu(Ss ,Sr ,u);
-u = ABu(Ry',Rx',u);
+	u = b .* Bi;
+	u = ABu(Ry ,Rx ,u);
+	u = ABu(Ssi,Sri,u);
+	u = u .* Di;
+	u = ABu(Ss ,Sr ,u);
+	u = ABu(Ry',Rx',u);
 
 end
