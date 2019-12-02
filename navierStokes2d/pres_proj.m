26bd1d47a1a97c4ac24b87d57797ff7ee8c1d998
Vedant Puri <vpuri3@illinois.edu>
2019-12-02 06:37:57 -0600

    pauses for debugging

--- navierStokes2d/pres_proj.m
+++ navierStokes2d/pres_proj.m
@@ -6,20 +6,27 @@ function [vx,vy,pr] = pres_proj(ux,uy,pr1,b0,Biv,Rxvx,Ryvx,Rxvy,Ryvy,slv...
 							,Sxp,Syp,Lip,Bv)
 
 	g = -diver(ux,uy,Bv,Jr,Js,Irv,Isv,Drv,Dsv,rxv,ryv,sxv,syv);
+	['in pres proj']
+	mesh(g),title('g'),pause
 
 	if(slv==1) % FDM
 		delp = b0 * fdm(g,Sxp,Syp,Lip);
 	end
-	
-	delp = delp - dot(Bp,delp)/dot(1+0*Bp,Bp);
+
+	%delp = delp - dot(Bp,delp)/dot(1+0*Bp,Bp);
+	mesh(delp),title('delp'),pause
 
 	[px,py] = vgradp(delp,Bv,Jr,Js,Irv,Isv,Drv,Dsv,rxv,ryv,sxv,syv);
+	mesh(px),title('px'),pause
+	mesh(py),title('py'),pause
 
-	px = (1/b0) * Biv .* ABu(Ryvx'*Ryvx,Rxvx'*Rxvx,px);
-	py = (1/b0) * Biv .* ABu(Ryvy'*Ryvy,Rxvy'*Rxvy,py);
+	dpvx = (1/b0) * Biv .* ABu(Ryvx'*Ryvx,Rxvx'*Rxvx,px);
+	dpvy = (1/b0) * Biv .* ABu(Ryvy'*Ryvy,Rxvy'*Rxvy,py);
+	mesh(dpvx),title('dpvx'),pause
+	mesh(dpvy),title('dpvy'),pause
 
-	vx = ux + px;
-	vy = uy + py;
+	vx = ux + dpvx;
+	vy = uy + dpvy;
 
 	pr = pr1 + delp;
 
