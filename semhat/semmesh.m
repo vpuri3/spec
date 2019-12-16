9353f8d8ec6b1472f2092bac3e360a62d0e6abfc
Vedant Puri <vpuri3@illinois.edu>
2019-12-16 14:48:27 -0600

    semq

---
+++ semhat/semmesh.m
@@ -0,0 +1,28 @@
+function [z] = semmesh(E,lx1,ifcheby)
+%
+% creates 1D sem mesh of poly. order lx1-1
+% and e elements [-1,1].
+%
+
+[z0,w0]=zwgll(lx1-1);
+z0=0.5*(z0+1); % [0,1]
+w0=0.5*w0;
+
+% element mesh
+if(ifcheby)
+	dtheta = (0:E)'*pi/E;
+	ze = -cos(dtheta);
+else
+	ze=(-1:2/E:1)'; % element mesh (size e+1)
+end
+
+z = kron(diff(ze),z0) + kron(ze(1:end-1),ones(lx1,1));
+z = unique(z);
+
+Q = semq(E,lx1-1,0);
+w = kron(diff(ze),w0);
+w = Q'*w;
+
+%figure;plot(z,z*0,'kx');grid on; sum(w)
+
+end
